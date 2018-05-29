6009eb8b4fe71519d47db6b3249916bb9b86d145
Amila Welihinda <amilajack@gmail.com>
2018-05-28 18:36:31 -0700

    Migrated to es6 imports

--- index.js
+++ index.js
@@ -1,30 +1,11 @@
-// This THREEx helper makes it easy to handle window resize.
-// It will update renderer and camera when window is resized.
-//
-// # Usage
-//
-// **Step 1**: Start updating renderer and camera
-//
-// ```var windowResize = THREEx.WindowResize(aRenderer, aCamera)```
-//    
-// **Step 2**: Start updating renderer and camera
-//
-// ```windowResize.stop()```
-// # Code
-
-
-
-/** @namespace */
-var THREEx	= THREEx || {};
-
 /**
  * Update renderer and camera when the window is resized
  * 
  * @param {Object} renderer the renderer to update
  * @param {Object} Camera the camera to update
 */
-THREEx.WindowResize	= function WindowResize(renderer, camera){
-	var callback	= function(){
+export default function WindowResize(renderer, camera){
+	var callback = function(){
 		// notify the renderer of the size change
 		renderer.setSize( window.innerWidth, window.innerHeight );
 		// update the camera
