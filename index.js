e71b59d50b90476560fcf136c82ede02eaf57317
Amila Welihinda <amilajack@gmail.com>
2018-05-28 18:37:45 -0700

    Modernized code

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 /**
  * Update renderer and camera when the window is resized
- * 
+ *
  * @param {Object} renderer the renderer to update
  * @param {Object} Camera the camera to update
 */
-export default function WindowResize(renderer, camera){
-	var callback = function(){
-		// notify the renderer of the size change
-		renderer.setSize( window.innerWidth, window.innerHeight );
-		// update the camera
-		camera.aspect	= window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
-	}
-	// bind the resize event
-	window.addEventListener('resize', callback, false);
-	// return .stop() the function to stop watching window resize
-	return {
-		/**
+export default function WindowResize(renderer, camera) {
+  const callback = () => {
+    // notify the renderer of the size change
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // update the camera
+    camera.aspect	= window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+  };
+
+  // bind the resize event
+  window.addEventListener('resize', callback, false);
+
+  // return .stop() the function to stop watching window resize
+  return {
+    /**
 		 * Stop watching window resize
-		*/
-		stop	: function(){
-			window.removeEventListener('resize', callback);
-		}
-	};
+		 */
+    stop() {
+      window.removeEventListener('resize', callback);
+    }
+  };
 }
